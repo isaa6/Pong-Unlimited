69d583e95b94687e962d043ce57f5195bbb9a455
Isaac Marovitz <isaacmarovitz@isaacs-macbook-2.local>
2017-08-06 12:37:03 +0100

    Initial Commit

---
+++ Pong UnlimitedTests/Pong_UnlimitedTests.swift
@@ -0,0 +1,36 @@
+//
+//  Pong_UnlimitedTests.swift
+//  Pong UnlimitedTests
+//
+//  Created by Isaac Marovitz on 06/08/2017.
+//  Copyright © 2017 Isaac Marovitz. All rights reserved.
+//
+
+import XCTest
+@testable import Pong_Unlimited
+
+class Pong_UnlimitedTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
